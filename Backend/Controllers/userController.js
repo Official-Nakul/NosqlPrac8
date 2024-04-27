942735ef46fbbc1efcefe7526509cc684cb6a20f
Official-Nakul <visavadianakulyt@gmail.com>
2024-04-27 10:21:50 +0530

    optimization

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -3,19 +3,12 @@ const { generateToken } = require("../jwt/jwt");
 const User = require("../Models/userSchema");
 
 const login = asynchandler(async (req, res) => {
-  const data = req.body;
-  console.log(data);
-  if (!data) {
-    return res.status(401).json({ error: "unauthorized" });
-  }
-  const user = await User.findOne({ user_name: data.username });
+  const { username, password } = req.body;
+  console.log(username, password);
+  const user = await User.findOne({ user_name: username, password: password });
   if (!user) {
-    return res.status(401).json({ error: "user not found" });
+    return res.status(401).send("invalid username or password");
   }
-  if (user.password !== data.password) {
-    return res.status(401).json({ error: "Incorrect password" });
-  }
-
   const payload = {
     username: user.user_name,
     uid: user._id,
